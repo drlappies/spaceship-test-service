ab23b2343a8a6d0aac47871924775c33c380eff3
mycl <76110050+drlappies@users.noreply.github.com>
2024-02-17 22:51:22 +0800

    fix: initial price list before cronjob (#5)

--- src/modules/price/price.schedular.ts
+++ src/modules/price/price.schedular.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CronExpression, Cron } from '@nestjs/schedule';
 import { Logger } from '@nestjs/common';
-import { CoingeckoService } from '../coingecko/coingecko.service';
-import { Coin, Currency } from '../coingecko/coingecko.type';
-import { PriceDto } from './dtos/price.dto';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { PriceEvent } from './price.event';
 import { PriceCache } from './price.cache';
+import { PriceService } from './price.service';
 
 @Injectable()
 export class PriceSchedular {
   constructor(
-    private readonly coingeckoService: CoingeckoService,
+    private readonly priceService: PriceService,
     private readonly logger: Logger,
     private readonly eventEmitter: EventEmitter2,
     private readonly priceCache: PriceCache,
@@ -20,34 +18,12 @@ export class PriceSchedular {
   @Cron(CronExpression.EVERY_MINUTE)
   async getCryptoPrice(): Promise<void> {
     this.logger.log(`getCryptoPrice ${CronExpression.EVERY_MINUTE}`);
-    const simplePrice = await this.coingeckoService.getSimplePrice({
-      ids: [
-        Coin.BITCOIN,
-        Coin.ETHERERUM,
-        Coin.LITECOIN,
-        Coin.MONERO,
-        Coin.RIPPLE,
-        Coin.DOGECOIN,
-        Coin.DASH,
-        Coin.MAIDSAFECOIN_TOKEN,
-        Coin.LISK,
-        Coin.STORJ,
-      ],
-      vs_currencies: [Currency.USD],
-      include_24hr_vol: true,
-      include_last_updated_at: true,
-      include_24hr_change: true,
-      precision: 8,
-    });
+    const priceList = await this.priceService.getExternalPriceData();
 
-    const priceDtos = Object.entries(simplePrice).map(
-      ([coin, details]) => new PriceDto(coin, details),
-    );
+    this.logger.debug(priceList);
 
-    this.logger.debug(priceDtos);
+    this.eventEmitter.emit(PriceEvent.PRICE_UPDATED, priceList);
 
-    this.eventEmitter.emit(PriceEvent.PRICE_UPDATED, priceDtos);
-
-    await this.priceCache.setPriceListCache(priceDtos);
+    await this.priceCache.setPriceListCache(priceList);
   }
 }
--- src/modules/price/price.service.ts
+++ src/modules/price/price.service.ts
@@ -1,16 +1,56 @@
 import { Injectable } from '@nestjs/common';
-import { PriceListEntity, PriceEntity } from './price.entity';
+import { PriceListEntity } from './price.entity';
 import { PriceCache } from './price.cache';
+import { CoingeckoService } from '../coingecko/coingecko.service';
+import { Coin, Currency } from '../coingecko/coingecko.type';
+import { PriceDto } from './dtos/price.dto';
 
 @Injectable()
 export class PriceService {
-  constructor(private readonly priceCache: PriceCache) {}
+  constructor(
+    private readonly priceCache: PriceCache,
+    private readonly coingeckoService: CoingeckoService,
+  ) {}
 
-  async getAll(): Promise<PriceListEntity> {
-    const priceList = await this.priceCache.getPriceListCache();
+  async getPriceList(): Promise<PriceListEntity> {
+    const cachedPriceList = await this.priceCache.getPriceListCache();
 
-    return new PriceListEntity({
-      data: priceList.map(({ coin, data }) => new PriceEntity({ coin, data })),
+    if (cachedPriceList) {
+      return cachedPriceList;
+    }
+
+    const priceList = await this.getExternalPriceData();
+
+    await this.priceCache.setPriceListCache(priceList);
+
+    return priceList;
+  }
+
+  async getExternalPriceData(): Promise<PriceListEntity> {
+    const simplePrice = await this.coingeckoService.getSimplePrice({
+      ids: [
+        Coin.BITCOIN,
+        Coin.ETHERERUM,
+        Coin.LITECOIN,
+        Coin.MONERO,
+        Coin.RIPPLE,
+        Coin.DOGECOIN,
+        Coin.DASH,
+        Coin.MAIDSAFECOIN_TOKEN,
+        Coin.LISK,
+        Coin.STORJ,
+      ],
+      vs_currencies: [Currency.USD],
+      include_24hr_vol: true,
+      include_last_updated_at: true,
+      include_24hr_change: true,
+      precision: 8,
     });
+
+    const priceDtos = Object.entries(simplePrice).map(
+      ([coin, details]) => new PriceDto(coin, details),
+    );
+
+    return new PriceListEntity({ data: priceDtos });
   }
 }
